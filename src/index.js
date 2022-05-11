822a2bb84a5295ac108ab46546475f4d68f24895
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-05-11 15:16:42 +0200

    adding  files to git and done with linters

--- src/index.js
+++ src/index.js
@@ -1,74 +1,30 @@
-// css files here
-import * as task from './populate.js';
-import * as stat from './status.js';
+// import _ from 'lodash';
+// import printMe from './print.js';
 import './style.css';
 
-let list = [];
-const listEl = document.querySelector('ul');
+const todo = [
+  { descr: 'my first to do list ',
+   comple: true,
+    index: 0 },
+  { descr: 'my second to do list ',
+   comple: false, 
+    index: 1 },
+];
 
 function todoList() {
-  if (window.localStorage.getItem('localTasks')) {
-    const localTasks = window.localStorage.getItem('localTasks');
-    list = JSON.parse(localTasks);
-  }
-  document.querySelector('.todo-list').innerHTML = '';
-  list.forEach((item) => {
-    const taskElement = document.createElement('li');
-    taskElement.classList.add('task');
-    if (item.isCompleted) {
-      taskElement.classList.add('completed');
+  let todoLi = '';
+  todo.forEach((item) => {
+    let checker;
+    let strike;
+    if (item.comple === true) {
+      checker = 'checked';
+      strike = 'strike-through';
     }
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.classList.add('task-check');
-    checkbox.addEventListener('click', () => {
-      stat.status(item, list);
-      todoList();
-    });
-    checkbox.checked = item.isCompleted;
-    taskElement.appendChild(checkbox);
-    const taskText = document.createElement('input');
-    taskText.classList = 'task-text';
-    taskText.value = item.description;
-    taskText.addEventListener('change', () => {
-      if (taskText.value.length > 0) {
-        item.description = taskText.value;
-        stat.saveLocal(list);
-      }
-    });
-    taskElement.appendChild(taskText);
-
-    const dragIcon = document.createElement('button');
-    dragIcon.classList = 'far fa-trash-alt deleteBtn';
-    taskElement.appendChild(dragIcon);
-    taskElement.draggable = 'true';
-    document.querySelector('.todo-list').appendChild(taskElement);
+    todoLi += `<li class="task">
+    <input class="task-check" type="checkbox" ${checker}>
+    <span class="list ${strike}" >${item.descr}</span>
+    <i class='fa fa-ellipsis-v' style="margin-left:auto"></i></li>`;
   });
+  document.querySelector('.todo-list').innerHTML = todoLi;
 }
-
-function removeItem(e) {
-  if (!e.target.classList.contains('deleteBtn')) {
-    return;
-  }
-  const btn = e.target;
-  list.forEach((task) => {
-    if (task.description === btn.parentElement.children[1].value) {
-      list.splice(list.indexOf(task), 1);
-    }
-  });
-  btn.closest('li').remove();
-  task.updateIndex(list);
-  stat.saveLocal(list);
-}
-
-listEl.addEventListener('click', removeItem);
 todoList();
-document.querySelector('#taskForm').addEventListener('submit', (event) => {
-  event.preventDefault();
-  task.add(list);
-  todoList();
-});
-document.querySelector('.clearer').addEventListener('click', () => {
-  task.removeDone(list);
-  todoList();
-});
--- src/print.js
+++
@@ -1,3 +0,0 @@
-export default function print() {
-    console.log('I get called from print.js!');
-  }
